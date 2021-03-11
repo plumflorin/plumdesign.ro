bace6cc6c2a09f2926aee2a84ba2f7f87fe3ca3a
plumflorin <40239311+plumflorin@users.noreply.github.com>
2021-03-11 10:43:20 +0200

    Final 1

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ const links = document.querySelectorAll(".nav-links li");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
-  links.forEach(link => {
+  links.forEach((link) => {
     link.classList.toggle("fade");
   });
 });
+
+if ($(".project").length > 0) {
+  $(".project").hover3d({
+    selector: ".project__card",
+  });
+}
